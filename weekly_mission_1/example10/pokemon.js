5b2e7ac318664afedfc123aab61f623245bed318
Luis <luisma0330@gmail.com>
2022-04-10 12:12:38 -0500

    Ejercicio 10

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,14 @@
+export default class MyPokemon {
+    constructor (name) {
+      this.name = name
+    }
+
+    sayHello () {
+        console.log(`Mi pokémon ${this.name} te saluda!!`)
+    }
+
+    sayMessage(message){
+        console.log(`Mi pokémon ${this.name} dice: ${message}`)
+    }
+}
+
