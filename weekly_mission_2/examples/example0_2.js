78d37a182d44968a455f5e919468780bb9559456
Luis <luisma0330@gmail.com>
2022-04-13 21:09:24 -0500

    Error de git corregido

---
+++ weekly_mission_2/examples/example0_2.js
@@ -0,0 +1,8 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    name: "Luis",
+    age: 26
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  
+  console.log(myObjetc2)
