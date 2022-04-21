2a48146780c0be8bf7e02838ce8f4eb7652f1d1e
Luis Ojeda <luisma0330@hotmail.com>
2022-04-20 22:30:42 -0500

    Uso de findIndex

---
+++ weekly_mission_2/operadores/example_13.js
@@ -0,0 +1,4 @@
+// Ejemplo 13: Uso de findIndex, este método regresa la posición del primer elemento que cumpla con la validación que indiques.
+const names13 = ['luis', 'Carlos', 'Issaac', 'Explorer 1']
+const result = names13.findIndex((name) => name.length > 7)
+console.log("Ejemplo 13: Primer elemento cuya palabra sea mayor a 7 esta en la posición "+ result)
