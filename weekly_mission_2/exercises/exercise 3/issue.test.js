efc882a2d3428797f760979431c37478247d5e1a
Luis Ojeda <luisma0330@hotmail.com>
2022-04-29 11:41:20 -0500

    Añadiendo una segunda prueba para la clase issue

--- weekly_mission_2/exercises/exercise 3/issue.test.js
+++ weekly_mission_2/exercises/exercise 3/issue.test.js
@@ -4,3 +4,8 @@ test('1) Create a new object Issue', () => {
     const myIssue = new Issue('Bug 1', 'Launch X', 'Active', 2, 5, 'luisma0330', '20/03/2022', '25/04/2022')
     expect(myIssue.tittle).toBe('Bug 1')
 });
+
+test('2) The object contain the status active', () => {
+    const myIssue = new Issue('Bug 1', 'Launch X', 'Active', 2, 5, 'luisma0330', '20/03/2022', '25/04/2022')
+    expect(myIssue.status).toBe('Active')
+});
